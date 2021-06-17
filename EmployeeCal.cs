624ade929a331c78d16284cdfe0631cce27edd73
Harsh <harsh.21483@gmail.com>
2021-06-17 01:26:26 -0700

    [Harsh]Refactor:-Wage for each company is shown with all attributes

--- EmployeeCal.cs
+++ EmployeeCal.cs
@@ -4,28 +4,39 @@ using System.Text;
 
 namespace EmployeeWageOOP
 {
-    class Employee
+    public class Employee
     {
+        private string company;
+        private int Wage_Per_Hr;
+        private int numWorkingDays;
+        private int MaxHrsPerMonth;
+        private int totalWage;
+
+        public Employee(string company,int Wage_Per_Hr,int numWorkingDays,int MaxHrsPerMonth)
+        {
+            this.company = company;
+            this.Wage_Per_Hr = Wage_Per_Hr;
+            this.numWorkingDays = numWorkingDays;
+            this.MaxHrsPerMonth = MaxHrsPerMonth;
+        }
         public void ComputeEmpWage()
         {
             //constants
-            const int WAGE_PER_HR = 20;
-            const int WORKING_DAYS = 20;
             const int IS_FULL_TIME = 8;
             const int IS_PART_TIME = 4;
             
-            
+
+
 
             //variables
             int dailyWage = 0;
             int day ;
             int isPresent;
-            int totalWage = 0;
             int totalHours = 0;
 
             //random number generation
             Random rand = new Random();
-            for (day = 1; day <= WORKING_DAYS; day++)
+            for (day = 1; day <= this.numWorkingDays; day++)
             {
                 isPresent = rand.Next(0, 3);
 
@@ -34,35 +45,45 @@ namespace EmployeeWageOOP
                     case 1:
                         {
                             Console.WriteLine("The employer is full time");
-                            dailyWage = WAGE_PER_HR * IS_FULL_TIME;
+                            dailyWage = this.Wage_Per_Hr * IS_FULL_TIME;
                             Console.WriteLine($"Daily wage is {dailyWage}");
                             break;
                         }
                     case 2:
                         {
                             Console.WriteLine("The employer is Part time");
-                            dailyWage = WAGE_PER_HR * IS_PART_TIME;
+                            dailyWage = this.Wage_Per_Hr * IS_PART_TIME;
                             Console.WriteLine($"Daily wage is {dailyWage}");
                             break;
                         }
                     default:
                         {
-                            Console.WriteLine($"Daily wage is {dailyWage}");
+                            dailyWage = isPresent;
                             break;
                         }
                 }
                 //checking total number of hours
                 totalHours += dailyWage / 20;
                 totalWage += dailyWage;
-                if (totalHours >= 100)
-                {
+                if (totalHours >= this.MaxHrsPerMonth)
                     break;
-                }
-                Console.WriteLine($"Montly wage is {totalWage} and working hours is {totalHours}"); // output 
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Company Name :{this.company}");
+            Console.WriteLine($"No. of hours worked :{totalHours}");
+            Console.WriteLine($"Wage Per hour :{this.Wage_Per_Hr}");
+            Console.WriteLine($"Monthly wage :{this.totalWage}");
+
+            
+            }
+            public string Result()
+            {
+            return "Total Employee wage for Comapny : " + this.company + " is " + this.totalWage;
             }
 
-        }
 
     }
+
 }
+
 
--- Program.cs
+++ Program.cs
@@ -9,8 +9,13 @@ namespace EmployeeWageOOP
             
             Console.WriteLine("Welcome to Employee wage Calculation ");
             //calculate employee Wage
-            Employee employee = new Employee();
-            employee.ComputeEmpWage();
+            Employee Google = new Employee("Google",300,20,70);
+            Employee Infosys = new Employee("Infosys", 100, 10, 30);
+            Google.ComputeEmpWage();
+            Console.WriteLine(Google.Result());
+            Infosys.ComputeEmpWage();
+            Console.WriteLine(Infosys.Result());
+            
             
         }
     }
